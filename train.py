67f6074f478356c540cf148895f8b9f4c6422fa6
leeminsun1205 <minhnhut12052004@gmai.com>
2025-04-22 11:46:26 +0700

    update args for train.py

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import json
+import argparse
 
 import torch
 import torch.nn as nn
@@ -8,65 +9,77 @@ from torchvision import transforms, datasets
 import torch.optim as optim
 from tqdm import tqdm
 
-from Medmamba import VSSM as medmamba # import model
+from Medmamba import VSSM as medmamba  # import model
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train a Medmamba model.')
+    parser.add_argument('--train_dir', type=str, required=True, help='Path to training dataset.')
+    parser.add_argument('--val_dir', type=str, required=True, help='Path to validation dataset.')
+    parser.add_argument('--num_classes', type=int, required=True, help='Number of output classes.')
+    parser.add_argument('--model_name', type=str, default='Medmamba', help='Model name for saving.')
+    parser.add_argument('--batch_size', type=int, default=32, help='Batch size.')
+    parser.add_argument('--epochs', type=int, default=100, help='Number of training epochs.')
+    parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate.')
+    return parser.parse_args()
 
 
 def main():
+    args = parse_args()
+
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print("using {} device.".format(device))
+    print("Using {} device.".format(device))
 
     data_transform = {
-        "train": transforms.Compose([transforms.RandomResizedCrop(224),
-                                     transforms.RandomHorizontalFlip(),
-                                     transforms.ToTensor(),
-                                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]),
-        "val": transforms.Compose([transforms.Resize((224, 224)),
-                                   transforms.ToTensor(),
-                                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])}
+        "train": transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ]),
+        "val": transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+    }
 
-    train_dataset = datasets.ImageFolder(root="the path of your train set",
-                                         transform=data_transform["train"])
+    train_dataset = datasets.ImageFolder(root=args.train_dir, transform=data_transform["train"])
     train_num = len(train_dataset)
 
-    flower_list = train_dataset.class_to_idx
-    cla_dict = dict((val, key) for key, val in flower_list.items())
-    # write dict into json file
-    json_str = json.dumps(cla_dict, indent=4)
+    # class index mapping
+    cla_dict = {val: key for key, val in train_dataset.class_to_idx.items()}
     with open('class_indices.json', 'w') as json_file:
-        json_file.write(json_str)
+        json.dump(cla_dict, json_file, indent=4)
 
-    batch_size = 32
-    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
-    print('Using {} dataloader workers every process'.format(nw))
+    nw = min([os.cpu_count(), args.batch_size if args.batch_size > 1 else 0, 8])
+    print(f'Using {nw} dataloader workers every process')
 
-    train_loader = torch.utils.data.DataLoader(train_dataset,
-                                               batch_size=batch_size, shuffle=True,
-                                               num_workers=nw)
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=nw)
 
-    validate_dataset = datasets.ImageFolder(root="the path of your validation set",,
-                                            transform=data_transform["val"])
-    val_num = len(validate_dataset)
-    validate_loader = torch.utils.data.DataLoader(validate_dataset,
-                                                  batch_size=batch_size, shuffle=False,
-                                                  num_workers=nw)
-    print("using {} images for training, {} images for validation.".format(train_num,
-                                                                           val_num))
+    val_dataset = datasets.ImageFolder(root=args.val_dir, transform=data_transform["val"])
+    val_num = len(val_dataset)
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=nw)
 
+    print(f"Using {train_num} images for training, {val_num} images for validation.")
 
-    net = medmamba(num_classes=num_classes)
+    net = medmamba(num_classes=args.num_classes)
     net.to(device)
-    loss_function = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(net.parameters(), lr=0.0001)
 
-    epochs = 100
+    loss_function = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=args.lr)
+
     best_acc = 0.0
-    save_path = './{}Net.pth'.format(model_name)
+    save_path = f'./{args.model_name}_best.pth'
     train_steps = len(train_loader)
-    for epoch in range(epochs):
-        # train
+
+    for epoch in range(args.epochs):
         net.train()
         running_loss = 0.0
         train_bar = tqdm(train_loader, file=sys.stdout)
+
         for step, data in enumerate(train_bar):
             images, labels = data
             optimizer.zero_grad()
@@ -75,30 +88,25 @@ def main():
             loss.backward()
             optimizer.step()
 
-            # print statistics
             running_loss += loss.item()
+            train_bar.desc = f"train epoch[{epoch + 1}/{args.epochs}] loss:{loss:.3f}"
 
-            train_bar.desc = "train epoch[{}/{}] loss:{:.3f}".format(epoch + 1,
-                                                                     epochs,
-                                                                     loss)
-
-        # validate
+        # validation
         net.eval()
-        acc = 0.0  # accumulate accurate number / epoch
+        acc = 0.0
         with torch.no_grad():
-            val_bar = tqdm(validate_loader, file=sys.stdout)
+            val_bar = tqdm(val_loader, file=sys.stdout)
             for val_data in val_bar:
                 val_images, val_labels = val_data
                 outputs = net(val_images.to(device))
                 predict_y = torch.max(outputs, dim=1)[1]
                 acc += torch.eq(predict_y, val_labels.to(device)).sum().item()
 
-        val_accurate = acc / val_num
-        print('[epoch %d] train_loss: %.3f  val_accuracy: %.3f' %
-              (epoch + 1, running_loss / train_steps, val_accurate))
+        val_accuracy = acc / val_num
+        print(f'[epoch {epoch + 1}] train_loss: {running_loss / train_steps:.3f}  val_accuracy: {val_accuracy:.3f}')
 
-        if val_accurate > best_acc:
-            best_acc = val_accurate
+        if val_accuracy > best_acc:
+            best_acc = val_accuracy
             torch.save(net.state_dict(), save_path)
 
     print('Finished Training')
