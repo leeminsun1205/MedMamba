200659a36534556fd92aa8bc9fb3f4e181b9146c
leeminsun1205 <minhnhut12052004@gmai.com>
2025-04-24 01:01:07 +0700

    update

--- MedMamba.py
+++ MedMamba.py
@@ -9,7 +9,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.checkpoint as checkpoint
 from einops import rearrange, repeat
-from timm.models.layers import DropPath, to_2tuple, trunc_normal_
+from timm.layers import DropPath, to_2tuple, trunc_normal_
 from mamba_ssm.ops.selective_scan_interface import selective_scan_fn, selective_scan_ref
 
 
